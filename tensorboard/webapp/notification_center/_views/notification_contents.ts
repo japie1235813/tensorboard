baabd0d5ab4ed4ebb5ffd5591a4b2f74228cec53
Jie-Wei Wu <jieweiwu@google.com>
2021-03-03 13:08:16 -0800

    display notifcation contents

---
+++ tensorboard/webapp/notification_center/_views/notification_contents.ts
@@ -0,0 +1,17 @@
+export const notifications = [
+  {
+    category: 'What’s new',
+    date: 1614704917196,
+    title: '2.2.9 Release:',
+    content: '* Revamped UX * Autocomplete in tag filter',
+    images: [],
+  },
+  {
+    category: 'What’s new',
+    date: 1614704917196,
+    title: '2.2.9 Release:',
+    content: '* Revamped UX *Autocomplete in tag filter',
+    images: [],
+  },
+
+];
