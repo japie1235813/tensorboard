97f2313c8b56e481e39e7b560478f682a21d53e1
Jie-Wei Wu <jieweiwu@google.com>
2021-03-03 09:23:09 -0800

    show notification content and styling

--- tensorboard/webapp/notification_center/_views/views_module.ts
+++ tensorboard/webapp/notification_center/_views/views_module.ts
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
@@ -25,6 +26,6 @@ import {NotificationCenterContainer} from './notification_center_container';
 @NgModule({
   declarations: [NotificationCenterContainer, NotificationCenterComponent],
   exports: [NotificationCenterContainer],
-  imports: [MatIconModule, MatMenuModule],
+  imports: [CommonModule, MatIconModule, MatMenuModule],
 })
 export class NotificationCenterViewModule {}
